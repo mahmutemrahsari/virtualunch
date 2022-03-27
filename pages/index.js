8a42ee6f18fda6716bf7c1dc117cf214f315f819
mahmutemrahsari <mahmutemrahsari@gmail.com>
2022-03-28 00:31:29 +0200

    data fetch added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react/cjs/react.production.min'
 import styles from '../styles/Home.module.css'
+
+
+async function getServerSideProps(res, data) {
+  res = await fetch(`http://localhost:3000/api/hello`)
+  data = await res.json()
+  if (!res.ok) {
+    // If there is a server error, you might want to
+    // throw an error instead of returning so that the cache is not updated
+    // until the next successful request.
+    throw new Error(`Failed to fetch posts, received status ${res.status}`)
+  }
+  console.log("DATA FROM SERVER===>>>", data)
+}
+
+const _alertMe = () => {
+  alert("RED ALERT")
+}
 
 export default function Home() {
   return (
@@ -13,43 +31,24 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to VirtuLunch
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Choose your random lunch mate :)
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div>Here is the employeers</div>
+          <div><button onClick={getServerSideProps}>Get Data</button></div>
         </div>
+
+        <div className={styles.grid}>
+
+          <div>{ }</div>
+        </div>
+
+
       </main>
 
       <footer className={styles.footer}>
